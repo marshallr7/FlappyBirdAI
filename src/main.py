008ccd114527da028449d787d93e5470b0461871
Marshall Patterson <marspatt@ttu.edu>
2022-09-29 08:35:23 -0500

    background and base "game" handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
 import pygame
+import const
 
-if __name__ == "__main__":
-    print("Hello World!")
-    print(f"PyGame Version: {pygame.version.ver}")
+game = pygame.display.set_mode((const.WIDTH, const.HEIGHT))
+pygame.display.set_caption("Flappy Bird AI")
+clock = pygame.time.Clock()
+clock.tick(30)
+
+background = pygame.image.load("../assets/bg.png")
+background = pygame.transform.scale(background, (const.WIDTH, const.HEIGHT))
+bird = pygame.image.load("../assets/bird1.png")
+
+i = 0
+
+while True:
+    bird_rect = bird.get_rect()
+    game.blit(background, (i, 0))
+    game.blit(background, (const.WIDTH+i, 0))
+
+    if i == -const.WIDTH:
+        i = 0
+        game.blit(background, (const.WIDTH + i, 0))
+    i -= 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("space")
+        pygame.display.update()
