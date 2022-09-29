e4f141764ead0e2a21e6d4498df70da832b8eab1
Marshall Patterson <marspatt@ttu.edu>
2022-09-29 17:00:30 -0500

    Adds bird class with minor functionality

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
 import pygame
 import const
 
+
+class Bird:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.height = y
+        self.image = image
+
+    def jump(self, y):
+        self.height = y
+
+    def get_image(self):
+        return self.image
+
+
 game = pygame.display.set_mode((const.WIDTH, const.HEIGHT))
 pygame.display.set_caption("Flappy Bird AI")
 clock = pygame.time.Clock()
@@ -8,14 +22,14 @@ clock.tick(30)
 
 background = pygame.image.load("../assets/bg.png")
 background = pygame.transform.scale(background, (const.WIDTH, const.HEIGHT))
-bird = pygame.image.load("../assets/bird1.png")
+bird = Bird(50, 500, pygame.image.load("../assets/bird1.png"))
 
 i = 0
 
 while True:
-    bird_rect = bird.get_rect()
     game.blit(background, (i, 0))
-    game.blit(background, (const.WIDTH+i, 0))
+    game.blit(background, (const.WIDTH + i, 0))
+    game.blit(bird.get_image(), (bird.x, bird.height))
 
     if i == -const.WIDTH:
         i = 0
@@ -26,5 +40,5 @@ while True:
             exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                print("space")
+                bird.jump(bird.height + -25)
         pygame.display.update()
